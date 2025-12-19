a4772445467d422f7b68522236487cf3196bdde4
marian <marian.muller@serli.com>
2025-12-19 11:59:16 +0100

    no reset irq (wip: somewhat breaks held time counter?) + visual feedback to reset + separate render functions

--- main.c
+++ main.c
@@ -11,6 +11,17 @@
 
 #define FB_COUNT 3
 
+T3DMat4FP* modelMatFP;
+T3DViewport viewport;
+T3DVec3 camPos = {{0,10.0f,40.0f}};
+T3DVec3 camTarget = {{0,0,0}};
+uint8_t colorAmbient[4] = {80, 80, 100, 0xFF};
+uint8_t colorDir[4]     = {0xEE, 0xAA, 0xAA, 0xFF};
+T3DVec3 lightDirVec = {{-1.0f, 1.0f, 1.0f}};
+T3DModel *model;
+rspq_block_t *dplDraw;
+int frameIdx = 0;
+
 // Keep rotation angle in persistent ram to survive reset without explicit backup/restore
 float rotAngle __attribute__((section(".persistent")));
 
@@ -34,11 +45,12 @@ typedef struct {
 } console_t;
 
 uint32_t stored = 0;
+uint32_t held_ms;
 
 volatile uint32_t reset_held __attribute__((section(".persistent")));
 
 // Callback for NMI/Reset interrupt
-static void reset_interrupt_callback(void) {
+/*static void reset_interrupt_callback(void) {
 	// There's a minimum guaranteed of 200ms (up to 500ms) before the console actually resets the game
 	// Reset does NOT happen if the player holds the reset button
 	reset_held = exception_reset_time();
@@ -50,6 +62,62 @@ static void reset_interrupt_callback(void) {
 	while (true) {
 		reset_held = exception_reset_time();
 	}
+}*/
+
+void render_3d(bool in_reset) {
+	// ======== Update ======== //
+	frameIdx = (frameIdx + 1) % FB_COUNT;
+
+	rotAngle -= in_reset ? 0.1f : 0.02f;
+	float modelScale = 0.1f;
+
+	t3d_viewport_set_projection(&viewport, T3D_DEG_TO_RAD(85.0f), 10.0f, 150.0f);
+	t3d_viewport_look_at(&viewport, &camPos, &camTarget, &(T3DVec3){{0,1,0}});
+
+	t3d_mat4fp_from_srt_euler(
+		&modelMatFP[frameIdx],
+		(float[3]){modelScale, modelScale, modelScale},
+		(float[3]){0.0f, rotAngle*0.2f, rotAngle},
+		(float[3]){0,0,0}
+	);
+
+	// ======== Draw (3D) ======== //
+	rdpq_attach(display_get(), display_get_zbuf());
+	t3d_frame_start();
+	t3d_viewport_attach(&viewport);
+
+	t3d_screen_clear_color(RGBA32(100, 80, 80, 0xFF));
+	t3d_screen_clear_depth();
+
+	t3d_light_set_ambient(colorAmbient);
+	t3d_light_set_directional(0, colorDir, &lightDirVec);
+	t3d_light_set_count(1);
+
+	//rdpq_set_prim_color(get_rainbow_color(rotAngle * 0.42f));
+
+	if(!dplDraw) {
+		rspq_block_begin();
+		t3d_model_draw(model);
+		t3d_matrix_pop(1);
+		dplDraw = rspq_block_end();
+	}
+
+	t3d_matrix_push(&modelMatFP[frameIdx]);
+	rspq_block_run(dplDraw);
+}
+
+void render_2d() {
+	// ======== Draw (2D) ======== //
+	rdpq_sync_pipe();
+
+	heap_stats_t stats;
+	sys_get_heap_stats(&stats);
+	rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 16, 210, "    Heap size : %d", stats.total);
+	rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 16, 220, "    Allocated : %d", stats.used);
+
+	rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 200, 200, "Stored  : %ld", stored);
+	rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 200, 210, "Reset held: %ldms", held_ms);
+	rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 200, 220, "FPS   : %.2f", display_get_fps());
 }
 
 int main(void) {
@@ -62,23 +130,16 @@ int main(void) {
 	joypad_init();
 	t3d_init((T3DInitParams){});
 
-	T3DMat4FP* modelMatFP = malloc_uncached(sizeof(T3DMat4FP) * FB_COUNT);
-	T3DViewport viewport = t3d_viewport_create_buffered(FB_COUNT);
+	modelMatFP = malloc_uncached(sizeof(T3DMat4FP) * FB_COUNT);
+	viewport = t3d_viewport_create_buffered(FB_COUNT);
   	rdpq_text_register_font(FONT_BUILTIN_DEBUG_MONO, rdpq_font_load_builtin(FONT_BUILTIN_DEBUG_MONO));
 
-	const T3DVec3 camPos = {{0,10.0f,40.0f}};
-	const T3DVec3 camTarget = {{0,0,0}};
-
-	uint8_t colorAmbient[4] = {80, 80, 100, 0xFF};
-	uint8_t colorDir[4]     = {0xEE, 0xAA, 0xAA, 0xFF};
-
-	T3DVec3 lightDirVec = {{-1.0f, 1.0f, 1.0f}};
 	t3d_vec3_norm(&lightDirVec);
 
-	T3DModel *model = t3d_model_load("rom:/model.t3dm");
+	model = t3d_model_load("rom:/model.t3dm");
 
-	rspq_block_t *dplDraw = NULL;
-	int frameIdx = 0;
+	dplDraw = NULL;
+	frameIdx = 0;
 
 	debugf("Stop N Swop Test ROM\n");
 
@@ -88,7 +149,7 @@ int main(void) {
 	if (rst == RESET_COLD) {
 		rotAngle = 0.0f;
 	}
-	uint32_t held_ms = (rst == RESET_COLD) ? 0 : TICKS_TO_MS(reset_held);
+	held_ms = (rst == RESET_COLD) ? 0 : TICKS_TO_MS(reset_held);
 	debugf("held_ms = %ld\n", held_ms);
 	
 	// Restore game data from heap replicas
@@ -97,72 +158,26 @@ int main(void) {
 
 	
 	// Reset IRQ handler
-	register_RESET_handler(reset_interrupt_callback);
+	//register_RESET_handler(reset_interrupt_callback);
 	
 	while (true) {
-		joypad_poll();
-		joypad_buttons_t pressed = joypad_get_buttons_pressed(JOYPAD_PORT_1);
-		if (pressed.a && stored < heap1.len) {
-			console_t* console = malloc(sizeof(console_t));
-			console->id = stored;
-			replicate(&heap1, CONSOLE_MAGIC | console->id, console, sizeof(console_t), 4, true, true);
-			stored++;
+		// TODO check exception_reset_time() to handle reset animation (fade to black? mask to black ? make console models react ??)
+		// TODO Alternate fast loop to be able to update reset_held with exception_reset_time() and measure reset button hold time?
+		reset_held = exception_reset_time();
+		bool in_reset = reset_held != 0;
+		if (!in_reset) {
+			joypad_poll();
+			joypad_buttons_t pressed = joypad_get_buttons_pressed(JOYPAD_PORT_1);
+			if (pressed.a && stored < heap1.len) {
+				console_t* console = malloc(sizeof(console_t));
+				console->id = stored;
+				replicate(&heap1, CONSOLE_MAGIC | console->id, console, sizeof(console_t), 4, true, true);
+				stored++;
+			}
 		}
 
-
-    	// ======== Update ======== //
-		frameIdx = (frameIdx + 1) % FB_COUNT;
-
-		rotAngle -= 0.02f;
-		float modelScale = 0.1f;
-
-		t3d_viewport_set_projection(&viewport, T3D_DEG_TO_RAD(85.0f), 10.0f, 150.0f);
-		t3d_viewport_look_at(&viewport, &camPos, &camTarget, &(T3DVec3){{0,1,0}});
-
-		t3d_mat4fp_from_srt_euler(
-			&modelMatFP[frameIdx],
-			(float[3]){modelScale, modelScale, modelScale},
-			(float[3]){0.0f, rotAngle*0.2f, rotAngle},
-			(float[3]){0,0,0}
-		);
-
-    	// ======== Draw (3D) ======== //
-		rdpq_attach(display_get(), display_get_zbuf());
-		t3d_frame_start();
-		t3d_viewport_attach(&viewport);
-
-		t3d_screen_clear_color(RGBA32(100, 80, 80, 0xFF));
-		t3d_screen_clear_depth();
-
-		t3d_light_set_ambient(colorAmbient);
-		t3d_light_set_directional(0, colorDir, &lightDirVec);
-		t3d_light_set_count(1);
-
-		//rdpq_set_prim_color(get_rainbow_color(rotAngle * 0.42f));
-
-		if(!dplDraw) {
-			rspq_block_begin();
-			t3d_model_draw(model);
-			t3d_matrix_pop(1);
-			dplDraw = rspq_block_end();
-		}
-
-		t3d_matrix_push(&modelMatFP[frameIdx]);
-		rspq_block_run(dplDraw);
-
-		// ======== Draw (2D) ======== //
-		rdpq_sync_pipe();
-
-		heap_stats_t stats;
-		sys_get_heap_stats(&stats);
-    	rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 16, 210, "    Heap size : %d", stats.total);
-    	rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 16, 220, "    Allocated : %d", stats.used);
-
-		rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 200, 200, "Stored  : %ld", stored);
-		rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 200, 210, "Reset held: %ldms", held_ms);
-		rdpq_text_printf(NULL, FONT_BUILTIN_DEBUG_MONO, 200, 220, "FPS   : %.2f", display_get_fps());
-
-
+		render_3d(in_reset);
+		render_2d();
 		rdpq_detach_show();
 	}
 
